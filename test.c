bbc6f55e65f66d2d7222c14aaf7fa31423eec394
Lev Brouk <lev@synadia.com>
2023-07-03 14:49:36 -0700

    emptied tests

--- test.c
+++ test.c
@@ -5,19 +5,19 @@ int main() {
     int a = 10;
     int b = 5;
 
-    int sum = add(a, b);
-    if (sum == 15) {
-        printf("Addition test passed!\n");
-    } else {
-        printf("Addition test failed! Expected: %d, Actual: %d\n", 15, sum);
-    }
+    // int sum = add(a, b);
+    // if (sum == 15) {
+    //     printf("Addition test passed!\n");
+    // } else {
+    //     printf("Addition test failed! Expected: %d, Actual: %d\n", 15, sum);
+    // }
 
-    int difference = sub(a, b);
-    if (difference == 5) {
-        printf("Subtraction test passed!\n");
-    } else {
-        printf("Subtraction test failed! Expected: %d, Actual: %d\n", 5, difference);
-    }
+    // int difference = sub(a, b);
+    // if (difference == 5) {
+    //     printf("Subtraction test passed!\n");
+    // } else {
+    //     printf("Subtraction test failed! Expected: %d, Actual: %d\n", 5, difference);
+    // }
 
     return 0;
 }
